bc0f1ff67e9ac4fc889586af28597ddae1c2c827
vikas yadav <thejsdeveloper@gmail.com>
2022-03-07 23:16:48 +0800

    using index convention for image option

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { ImageOption } from "./src/components/ImageOption/ImageOption";
+import { ImageOption } from "./src/components/ImageOption";
 
 export default function App() {
   return (
